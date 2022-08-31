b4983ec4d40ff71f5e57f8da5ae751a13a0363f0
AlissonK1 <Alisson.kauan77@gmail.com>
2022-08-30 22:14:05 -0300

    Exercicio de Threads

--- Thread/Main.java
+++ Thread/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Teste");
+public class Main implements Runnable {
+    public void run() {
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(Thread.currentThread().getName() + "  " + i);
+            try {
+                Thread.sleep(1000);
+            }
+
+            catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        Thread t = Thread.currentThread();
+        System.out.println("Current thread: " + t.getName());
+        System.out.println("Is Alive? " + t.isAlive());
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Thread t = new Thread(new Main());
+        t.start();
+
+        Thread t2 = new Thread(new Main());
+        t2.start();
+
+        t.join(10000);
+        System.out.println("\nJoining after 10000" + " milliseconds: \n");
+        System.out.println("Current thread: " + t.getName());
+        System.out.println("Current thread: " + t2.getName());
+        System.out.println("Is alive? " + t.isAlive());
+        System.out.println("Is alive? " + t2.isAlive());
     }
 }
